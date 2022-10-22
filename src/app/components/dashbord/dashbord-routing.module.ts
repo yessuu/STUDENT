f544c4c2ecb7e54b00a2fe6669f2e118422f4b51
Yesica <yesicasunol@gmail.com>
2022-10-22 03:41:17 -0300

    generacion nuevas componentes y servicios

--- src/app/components/dashbord/dashbord-routing.module.ts
+++ src/app/components/dashbord/dashbord-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddStudentComponent } from './add-student/add-student.component';
+import { CursosComponent } from './cursos/cursos.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -9,9 +10,10 @@ import { TableStudentComponent } from './table-student/table-student.component';
 
 const routes: Routes = [
     { path: '', component: DashbordComponent, children:[
-        {path: 'home', component: HomeComponent},
-        {path: 'usuarios', component: TableStudentComponent},
-        {path: 'agregar', component: AddStudentComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'usuarios', component: TableStudentComponent},
+    {path: 'agregar', component: AddStudentComponent},
+    {path: 'cursos', component: CursosComponent},
     ]},  
 ];
 
---
+++ src/app/interfaces/cursos.ts
@@ -0,0 +1,10 @@
+import { Data } from "@angular/router";
+
+export interface Cursos {
+    length: number;
+    id: number;
+    nombre: string,
+    duracion: Data,
+    descripcion: string,
+    image:string,
+}
